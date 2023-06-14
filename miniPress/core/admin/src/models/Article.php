8286cc22d018443ba0f68e15196118a712811a80
mathis54300 <mathis54300fox@gmail.com>
2023-06-14 16:33:14 +0200

    Models et schema

---
+++ miniPress/core/admin/src/models/Article.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace miniPress\admin\models;
+
+class Article extends \Illuminate\Database\Eloquent\Model
+{
+    protected $table = 'Articles';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    public function categorie(){
+        return $this->belongsTo(Categorie::class, 'cat_id');
+    }
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+}
--- miniPress/sql/schema.sql
+++ miniPress/sql/schema.sql
@@ -1,32 +1,49 @@
-DROP TABLE IF EXISTS Articles;
-DROP TABLE IF EXISTS Categories;
-DROP TABLE IF EXISTS Users;
+-- Adminer 4.8.1 MySQL 5.5.5-10.11.3-MariaDB-1:10.11.3+maria~ubu2204 dump
 
-CREATE TABLE Users
-(
-    ID       INT AUTO_INCREMENT PRIMARY KEY,
-    Email    VARCHAR(255) NOT NULL,
-    Password VARCHAR(255) NOT NULL,
-    Role     ENUM('admin', 'editor') NOT NULL DEFAULT 'editor'
-);
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-CREATE TABLE Categories
-(
-    ID   INT AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(255) NOT NULL
-);
+SET NAMES utf8mb4;
 
-CREATE TABLE Articles
-(
-    ID           INT AUTO_INCREMENT PRIMARY KEY,
-    Title        VARCHAR(255) NOT NULL,
-    Summary      TEXT,
-    Content      TEXT         NOT NULL,
-    DateCreation DATETIME DEFAULT CURRENT_TIMESTAMP,
-    Category_ID  INT,
-    User_ID      INT,
-    Image_URL    VARCHAR(512),
-    IsPublished  BOOLEAN  DEFAULT 0,
-    FOREIGN KEY (Category_ID) REFERENCES Categories (ID),
-    FOREIGN KEY (User_ID) REFERENCES Users (ID)
-);
+DROP TABLE IF EXISTS `Articles`;
+CREATE TABLE `Articles` (
+                            `id` int(11) NOT NULL AUTO_INCREMENT,
+                            `title` varchar(255) NOT NULL,
+                            `summary` text DEFAULT NULL,
+                            `content` text DEFAULT NULL,
+                            `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+                            `cat_id` int(11) NOT NULL,
+                            `user_id` int(11) NOT NULL,
+                            `image_url` varchar(512) DEFAULT NULL,
+                            `isPublished` tinyint(1) NOT NULL DEFAULT 0,
+                            `published_at` datetime DEFAULT NULL,
+                            PRIMARY KEY (`id`),
+                            KEY `Category_ID` (`cat_id`),
+                            KEY `User_ID` (`user_id`),
+                            CONSTRAINT `Articles_ibfk_1` FOREIGN KEY (`cat_id`) REFERENCES `Categories` (`ID`),
+                            CONSTRAINT `Articles_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+DROP TABLE IF EXISTS `Categories`;
+CREATE TABLE `Categories` (
+                              `id` int(11) NOT NULL AUTO_INCREMENT,
+                              `name` varchar(255) NOT NULL,
+                              PRIMARY KEY (`id`),
+                              CONSTRAINT `Categories_ibfk_1` FOREIGN KEY (`id`) REFERENCES `Articles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+DROP TABLE IF EXISTS `Users`;
+CREATE TABLE `Users` (
+                         `id` int(11) NOT NULL AUTO_INCREMENT,
+                         `email` varchar(255) NOT NULL,
+                         `password` varchar(255) NOT NULL,
+                         `role` enum('admin','editor') NOT NULL DEFAULT 'editor',
+                         PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- 2023-06-14 14:22:27
