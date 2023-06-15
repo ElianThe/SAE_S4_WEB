d092187fcfaf6fe27f8677fb0f5001736a509b58
mathis54300 <mathis54300fox@gmail.com>
2023-06-15 14:56:34 +0200

    debug

--- miniPress/core/admin/src/conf/routes.php
+++ miniPress/core/admin/src/conf/routes.php
@@ -4,8 +4,6 @@ use miniPress\admin\actions\GetArticlesAction;
 use miniPress\admin\actions\GetCategoriesAction;
 use miniPress\admin\actions\AddUserAction;
 use miniPress\admin\actions\AddUserPostAction;
-use miniPress\admin\action\GetNewArticleAction;
-
 use miniPress\admin\actions\GetNewArticleAction;
 use miniPress\admin\actions\GetNewCategorieAction;
 use miniPress\admin\actions\PostNewArticleAction;
@@ -25,16 +23,16 @@ return function (App $app): void {
     $app->post('/categorie/new[/]', PostNewCategorieAction::class)->setName('createdCategorie');
 
     $app->get('/categorie[/]', GetCategoriesAction::class)->setName('categoriesList');
-};
+
     //1) Créer un article : affichage et traitement d’un formulaire de saisie d’un article comprenant le
     //titre, le résumé et le contenu de l’article. La date de création est automatiquement valorisée. Le
     //résumé et le contenu sont saisis et stockés en base de données au format markdown
 
     //route get
-    $app->get('/article/new[/]', GetNewArticleAction::class)->setName('admin');
     $app->get('/register[/]', AddUserAction::class)->setName('register');
 
     //route post
     $app->post('/register[/]', AddUserPostAction::class)->setName('registerPost');
 
+};
 
