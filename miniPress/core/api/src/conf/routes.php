cef9ebbaaf59aeef0f1ab5b08a8b1c30e8542611
Maxime48 <37154360+Maxime48@users.noreply.github.com>
2023-06-16 11:17:53 +0200

    Adding CSRF service + forgotten optional / in routes

--- miniPress/core/api/src/conf/routes.php
+++ miniPress/core/api/src/conf/routes.php
@@ -12,15 +12,15 @@ require_once __DIR__ . '/../vendor/autoload.php';
 return function (\Slim\App $app): void {
     $app->group('/api', function (RouteCollectorProxy $api) {
         $api->group('/categories', function (RouteCollectorProxy $categorie) {
-            $categorie->get('', GetCategoriesAction::class);
-            $categorie->get('/{id}/articles', GetCategorieArticlesActions::class);
+            $categorie->get('[/]', GetCategoriesAction::class);
+            $categorie->get('/{id}/articles[/]', GetCategorieArticlesActions::class);
         });
 
         $api->group('/articles', function (RouteCollectorProxy $articles) {
-            $articles->get('', GetArticlesAction::class);
+            $articles->get('[/]', GetArticlesAction::class);
             $articles->get('/{id}', GetArticleById::class);
         });
 
-        $api->get('/auteurs/{id}/articles', GetArticlesByAuteur::class);
+        $api->get('/auteurs/{id}/articles[/]', GetArticlesByAuteur::class);
     });
 };
