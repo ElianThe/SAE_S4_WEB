7945022260e5f6b987e415ea4c614ed98726beb1
Regis <vavasseu2u@etu.univ-lorraine.fr>
2023-06-16 10:52:03 +0200

    Ajout de la page profil d'un utilisateur

--- miniPress/core/admin/src/models/User.php
+++ miniPress/core/admin/src/models/User.php
@@ -8,6 +8,13 @@ class User extends \Illuminate\Database\Eloquent\Model
     protected $primaryKey = 'id';
     public $timestamps = false;
 
+    const EDITOR = 'editor';
+    const ADMIN = 'admin';
+    const ROLE = [
+        self::EDITOR => 'Editeur',
+        self::ADMIN => 'Administrateur',
+    ];
+
     public function articles(){
         return $this->hasMany(Article::class, 'user_id');
     }
