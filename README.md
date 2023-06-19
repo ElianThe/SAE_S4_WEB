48c2077c0bd281fc76bb0cd0a5f3ddfe5354d5a2
mathis54300 <mathis54300fox@gmail.com>
2023-06-19 17:14:29 +0200

    Tri par catégorie

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # SAE_S4_WEB
+
+# MyGiftBox.net
+
+- Groupe : MASSON Paul, SEILER Mathis , 
+
+## Installation
+
+Suivez ces étapes pour installer MyGiftBox :
+
+## Fonctionnalitées
+### Gestion des articles :
+- [x] Créer un article
+- [x] Créer un article en choisissant une catégorie
+- [x] Afficher la liste des articles
+- [] Afficher la liste des articles en filtrant par catégorie
+- [x] Création d’une catégorie
+### Gestion des utilisateurs :
+- [x] Formulaire d’authentification
+- [x] Contrôle d’accès
+- [x] Auteurs
+### API :
+- [] Liste des catégories
+- [] Liste des articles
+- [] articles d’une catégorie
+- [] article complet
+- [] Liste des articles d’un auteur
+### Fonctionnalités étendues :
+- [x] 
+- [x] 
+
--- miniPress/core/admin/src/actions/GetArticlesAction.php
+++ miniPress/core/admin/src/actions/GetArticlesAction.php
@@ -3,22 +3,42 @@
 namespace miniPress\admin\actions;
 
 use miniPress\admin\services\article\ArticleService;
+use miniPress\admin\services\article\CategorieNotFoundException;
+use miniPress\admin\services\categorie\CategorieService;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Routing\RouteContext;
 use Slim\Views\Twig;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class GetArticlesAction extends Action
 {
+    /**
+     * @throws SyntaxError
+     * @throws CategorieNotFoundException
+     * @throws RuntimeError
+     * @throws LoaderError
+     */
     public function __invoke(Request $rq, Response $rs, array $args): Response
     {
         $articlesService = new ArticleService();
+        $categorieService = new CategorieService();
+
+        $idCatTrier = $rq->getQueryParams()['tri'] ?? "";
+        if ($idCatTrier == "") {
+            $articles = $articlesService->getArticles();
+        } else {
+            $articles = $articlesService->getArticlesByCategorie($idCatTrier);
+        }
 
         $routeParser = RouteContext::fromRequest($rq)->getRouteParser();
 
         $view = Twig::fromRequest($rq);
         return $view->render($rs, 'GetArticlesView.twig', [
-            'articles' => $articlesService->getArticles(),
+            'articles' => $articles,
+            'categories' => $categorieService->getCategories(),
         ]);
     }
 }
