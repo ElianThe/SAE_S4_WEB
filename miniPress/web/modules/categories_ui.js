eacba887153aad3ad58caee324bb3800bbaa6067
Maxime48 <37154360+Maxime48@users.noreply.github.com>
2023-06-21 11:36:52 +0200

    Uses of classes for js + small api change

--- miniPress/web/modules/categories_ui.js
+++ miniPress/web/modules/categories_ui.js
@@ -1,31 +1,31 @@
-function displayCategories(categories) {
-    const categoriesContainer = document.createElement('div');
-    categoriesContainer.id = 'categories-container';
+class CategoriesUI {
+    displayCategories(categories) {
+        const categoriesContainer = document.createElement('div');
+        categoriesContainer.id = 'categories-container';
 
-    const categoriesList = document.createElement('ul');
-    categoriesList.classList.add('list-group', 'list-group-flush');
+        const categoriesList = document.createElement('ul');
+        categoriesList.classList.add('list-group', 'list-group-flush');
 
-    let categorie;
-    categories.categories.forEach(category => {
-        categorie = category.category;
-        const categoryItem = document.createElement('li');
-        categoryItem.classList.add('list-group-item');
+        let categorie;
+        categories.categories.forEach(category => {
+            categorie = category.category;
+            const categoryItem = document.createElement('li');
+            categoryItem.classList.add('list-group-item');
 
-        const categoryButton = document.createElement('div');
-        const categoryLink = document.createElement('p');
-        categoryLink.classList.add('link-primary', 'category', 'm-0');
-        categoryLink.style.cursor = 'pointer';
-        categoryLink.textContent = categorie.name;
-        categoryButton.appendChild(categoryLink);
+            const categoryButton = document.createElement('div');
+            const categoryLink = document.createElement('p');
+            categoryLink.classList.add('link-primary', 'category', 'm-0');
+            categoryLink.style.cursor = 'pointer';
+            categoryLink.textContent = categorie.name;
+            categoryButton.appendChild(categoryLink);
 
-        categoryItem.appendChild(categoryLink);
-        categoriesList.appendChild(categoryItem);
-    });
+            categoryItem.appendChild(categoryLink);
+            categoriesList.appendChild(categoryItem);
+        });
 
-    categoriesContainer.appendChild(categoriesList);
-    document.getElementById('sidebar').appendChild(categoriesContainer);
+        categoriesContainer.appendChild(categoriesList);
+        document.getElementById('sidebar').appendChild(categoriesContainer);
+    }
 }
 
-export default {
-    displayCategories
-}
+export default new CategoriesUI();
